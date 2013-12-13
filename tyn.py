d9ecb578df2958d8ef9cc3546f04cf85d4643adf
mhemeryck <martijn.hemeryck@soundtalks.com>
2013-12-13 12:27:29 +0100

    added dots to filename extensions

--- tyn.py
+++ tyn.py
@@ -34,7 +34,7 @@ def lsfigs(args):
                 d['path'] = path
                 d['base'] = base = os.path.basename(filename)
                 d['description'], ext = os.path.splitext(base)
-                if ext in ['png', 'jpg', 'jpeg', 'pdf']:
+                if ext in ['.png', '.jpg', '.jpeg', '.pdf']:
                     fmt = fig_fmt
                 else:
                     fmt = file_fmt
